4afdef1127127ee0de48a57c71502de7c17434ae
Denys Vitali <denys@denv.it>
2020-12-12 14:05:45 +0100

    utils: make get_file pub

--- utils/src/lib.rs
+++ utils/src/lib.rs
@@ -1,6 +1,6 @@
 use std::path::Path;
 
-pub(crate) fn get_file(file: &str, file_name : &str) -> String {
+pub fn get_file(file: &str, file_name : &str) -> String {
     Path::new(file).parent().unwrap()
         .join(format!("{}{}", "./input/", file_name))
         .to_str().unwrap().to_owned()
